9a1d13b04b6267c8dc9c6d17342d8a1cf4e51cb2
José Vinicius <josevbluz@outlook.com>
2022-03-28 19:17:19 -0300

    login-requisito-1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import Header from '../components/Header/Header';
+import Button from '../components/Button/Button';
 
 export default function Login() {
   return (
     <div>
-      Login
-      <Header />
+      <form>
+        <label htmlFor="loginInput">
+          Email:
+          <input type="text" name="login" data-testid="email-input" />
+        </label>
+        <label htmlFor="passwordInput">
+          Senha:
+          <input type="text" name="password" data-testid="password-input" />
+        </label>
+        <Button dataTestId="login-submit-btn" buttonName="Entrar" />
+      </form>
     </div>
   );
 }
