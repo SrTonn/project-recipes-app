2e4f490119efbf171859e421ed8d642b48c04bbd
José Vinicius <josevbluz@outlook.com>
2022-03-29 15:40:40 -0300

    Pagina de login, requisito 5 finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../components/Button/Button';
 
 export default function Login() {
+  const [inputText, setInputText] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [button, setButton] = useState({ isDisable: true });
+
+  useEffect(() => {
+    const validateLogin = () => {
+      const MAX_CHARACTER_LENGTH = 6;
+      const conditions = [
+        /^[a-z._-]{3,26}@[a-z]{3,16}\.[a-z]{2,3}$/i.test(inputText.email),
+        inputText.password.length >= MAX_CHARACTER_LENGTH,
+      ];
+      console.log(conditions);
+      console.log('input password:', inputText.password, inputText.password.length);
+
+      setButton({ isDisable: conditions.includes(false) });
+    };
+    validateLogin();
+  }, [inputText]);
+
+  const handleChange = ({ name, value }) => {
+    setInputText((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <form>
         <label htmlFor="loginInput">
           Email:
-          <input type="text" name="login" data-testid="email-input" />
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            onChange={ ({ target }) => handleChange(target) }
+            value={ inputText.email }
+          />
         </label>
         <label htmlFor="passwordInput">
           Senha:
-          <input type="text" name="password" data-testid="password-input" />
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            onChange={ ({ target }) => handleChange(target) }
+            value={ inputText.password }
+          />
         </label>
-        <Button dataTestId="login-submit-btn" buttonName="Entrar" />
+        <Button
+          dataTestId="login-submit-btn"
+          buttonName="Entrar"
+          isDisabled={ button.isDisable }
+        />
       </form>
     </div>
   );
