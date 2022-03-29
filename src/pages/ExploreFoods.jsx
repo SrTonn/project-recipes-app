226c7f7d26c30872632679203d3d2ad6e40eb35b
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-29 16:47:42 -0300

    footer-requisito-21-finalizado

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer/Footer';
 
 export default function ExploreFoods() {
   return (
-    <div>ExploreFoods</div>
+    <div>
+      ExploreFoods
+      <Footer />
+    </div>
   );
 }
