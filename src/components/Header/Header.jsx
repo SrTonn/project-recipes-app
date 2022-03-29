87091aad9cb4b964ad7c8ac73109b76cd1cec590
calilisantos <calilisantos@gmail.com>
2022-03-29 18:34:54 -0300

    header-requisito-10. Status: em refatoracao

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import Button from '../Button/Button';
@@ -7,29 +7,51 @@ import searchIcon from '../../images/searchIcon.svg';
 
 export default function Header() {
   const { push, location: { pathname } } = useHistory();
+  const [inputSearch, setInputSearch] = useState({
+    isVisible: false,
+    inputValue: '',
+  });
   const title = pathname.slice(1)[0].toUpperCase() + pathname.slice(2);
 
   const handleClick = () => {
     push('/profile');
   };
 
+  const toggleInput = () => {
+    setInputSearch((prevState) => ({ ...prevState, isVisible: !prevState.isVisible }));
+  };
+
+  const handleChange = ({ value }) => {
+    setInputSearch((prevState) => ({ ...prevState, inputValue: value }));
+  };
+
   return (
     <header>
       <Button
         dataTestId="profile-top-btn"
-        buttonName={ <img src={ profileIcon } alt="profile-icon" /> }
         handleClick={ handleClick }
         src="profileIcon"
-      />
+      >
+        <img src={ profileIcon } alt="profile-icon" />
+      </Button>
       <p data-testid="page-title">{title}</p>
       <Button
         dataTestId="search-top-btn"
-        buttonName={ <img
-          src={ searchIcon }
-          alt="searchIcon"
-        /> }
         src="searchIcon"
-      />
+        handleClick={ toggleInput }
+      >
+        <img src={ searchIcon } alt="searchIcon" />
+      </Button>
+      { inputSearch.isVisible
+      && <input
+        data-testid="search-input"
+        type="text"
+        id="search"
+        value={ inputSearch.inputValue }
+        onChange={ ({ target }) => {
+          handleChange(target);
+        } }
+      />}
     </header>
   );
 }
