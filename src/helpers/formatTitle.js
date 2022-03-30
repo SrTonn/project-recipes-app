fab6b7886f1ff2484784e458a78614e7d7ac0bc6
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-30 15:45:06 -0300

    correcao de lint

--- src/helpers/formatTitle.js
+++ src/helpers/formatTitle.js
@@ -2,7 +2,8 @@ const formatTitle = (input) => {
   if (input.match(/\//g).length > 2) {
     input = input.replace(/(?<!^)\/\w+/, '');
   }
-  return input.replace(/((\/|-)\w)/g, (_, firstLetter) => ` ${firstLetter[1].toUpperCase()}`);
+  return input
+    .replace(/((\/|-)\w)/g, (_, firstLetter) => ` ${firstLetter[1].toUpperCase()}`);
 };
 
 export default formatTitle;
