931e31a53b3c8b328e0c7b57107c745ff84a38cb
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-30 15:34:50 -0300

    criação da função 'formaTitle'

---
+++ src/helpers/formatTitle.js
@@ -0,0 +1,8 @@
+const formatTitle = (input) => {
+  if (input.match(/\//g).length > 2) {
+    input = input.replace(/(?<!^)\/\w+/, '');
+  }
+  return input.replace(/((\/|-)\w)/g, (_, firstLetter) => ` ${firstLetter[1].toUpperCase()}`);
+};
+
+export default formatTitle;
