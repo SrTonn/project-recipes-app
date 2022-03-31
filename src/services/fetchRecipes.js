977663996a796ecea55f8f64576b64e924a4002e
Marina Barros <marinabarrossilva@gmail.com>
2022-03-31 14:32:03 -0300

    barra-de-busca-tests-funcionamento-dos-elementos status: em andamento

--- src/services/fetchRecipes.js
+++ src/services/fetchRecipes.js
@@ -1,6 +1,7 @@
 const getRecipes = async (endPoint) => {
   const recipesFetched = await fetch(endPoint);
   const recipesTreated = await recipesFetched.json();
+  console.log('recipes treated na api', recipesTreated);
   return recipesTreated;
 };
 
