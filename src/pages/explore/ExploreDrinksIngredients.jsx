7b3dc93ac5cd9cfff92d252ccbc8ad45ec734640
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-04-05 23:02:19 -0300

    tela-de-explorar-ingredientes-requisito-75-76-77 status: finalizado

--- src/pages/explore/ExploreDrinksIngredients.jsx
+++ src/pages/explore/ExploreDrinksIngredients.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../../components/Card/Card';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import styles from '../../styles/pageExploreIngredients.module.css';
 
 export default function ExploreDrinksIngredients() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+      const { drinks } = await response.json();
+      const TWELVE = 12;
+      setData(drinks.slice(0, TWELVE));
+    })();
+  }, []);
+
   return (
-    <main>
+    <>
       <Header />
-      <div>ExploreDrinksIngredients</div>
+      <main className={ styles.Main }>
+        {console.log(data)}
+        {data && data.map(({ strIngredient1 }, index) => (
+          <Card
+            key={ strIngredient1 }
+            index={ index }
+            src={ `https://www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png` }
+            name={ strIngredient1 }
+            dataTestId={ {
+              container: '-ingredient-card',
+              img: '-card-img',
+              paragraph: '-card-name',
+            } }
+          />
+        ))}
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
--- src/pages/explore/ExploreFoodsIngredients.jsx
+++ src/pages/explore/ExploreFoodsIngredients.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../../components/Card/Card';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import styles from '../../styles/pageExploreIngredients.module.css';
 
 export default function ExploreFoodsIngredients() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+      const { meals } = await response.json();
+      const TWELVE = 12;
+      setData(meals.slice(0, TWELVE));
+    })();
+  }, []);
+
   return (
-    <main>
+    <>
       <Header />
-      <div>ExploreFoodsIngredients</div>
+      <main className={ styles.Main }>
+        {console.log(data)}
+        {data && data.map(({ idIngredient, strIngredient }, index) => (
+          <Card
+            key={ idIngredient }
+            index={ index }
+            src={ `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png` }
+            name={ strIngredient }
+            dataTestId={ {
+              container: '-ingredient-card',
+              img: '-card-img',
+              paragraph: '-card-name',
+            } }
+          />
+        ))}
+      </main>
       <Footer />
-    </main>
+    </>
+
   );
 }
